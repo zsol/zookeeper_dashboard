6d5648eed5a608b6cc53436677c09e9c43ff3e39
Attila Babo <attila.babo@gmail.com>
2013-06-16 18:59:34 +0200

    Remove unnecessary comments from urls.py

--- zookeeper_dashboard/urls.py
+++ zookeeper_dashboard/urls.py
@@ -1,21 +1,9 @@
 from django.conf import settings
 from django.conf.urls import include, patterns
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('zookeeper_dashboard',
     (r'^cluster/', include('zookeeper_dashboard.zkadmin.urls')),
     (r'^tree/', include('zookeeper_dashboard.zktree.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/(.*)', admin.site.root),
-
     (r'^$', include('zookeeper_dashboard.zkadmin.urls')),
 )
 
